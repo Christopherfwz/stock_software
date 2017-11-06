f79a486b3aebbc1b0422e0d2f24a3b1fefe1910c
AsrielMao <32787204+AsrielMao@users.noreply.github.com>
2017-11-06 11:29:08 +0800

    Update iwencai.py
    get stock code and news several days ago

--- stock_news/iwencai.py
+++ stock_news/iwencai.py
@@ -10,8 +10,26 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def getZtCode():
-    start_url = 'http://www.iwencai.com/stockpick/robot-search?w=%E6%B6%A8%E5%81%9C%E8%82%A1%E5%88%86%E6%9E%90&querytype=stock&robotResultPerpage=30'
+def getDate(daysAgo):
+	date_got = datetime.datetime.now() + datetime.timedelta(days=-int(daysAgo))
+	return date_got
+
+
+def corDate(Numb):
+	if int(Numb) < 10:
+		correctDate = '0' + str(Numb)
+	else:
+		correctDate = str(Numb)
+	return correctDate
+
+def dateToString(date):
+	date_string=str(date.year)+corDate(date.month)+corDate(date.day)
+	return date_string
+
+def getZtCode(date_required):
+    basic_url = 'http://www.iwencai.com/'
+    date_string = dateToString(date_required)
+    start_url=basic_url+'stockpick/robot-search?w=%E6%B6%A8%E5%81%9C%20'+date_string+'&querytype=stock&robotResultPerpage=30'
     req = urllib2.Request(start_url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0')
     html = urllib2.urlopen(req).read().decode('utf-8')
@@ -45,14 +63,7 @@ def getZtCode():
 
     return stockcode
 
-def corDate(Numb):
-	if int(Numb) < 10:
-		correctDate = '0' + str(Numb)
-	else:
-		correctDate = str(Numb)
-	return correctDate
-
-def getnews(search_key):
+def getnews(search_key,date_required):
     # make sure search_key and page is valid
     try:
         search_key = str(search_key)
@@ -67,10 +78,8 @@ def getnews(search_key):
     url = 'http://www.iwencai.com/'
 
     # set time range for news
-    date_now = datetime.datetime.now()
-    date_begin = datetime.datetime.now() + datetime.timedelta(days=-3)
-    d_begin = str(date_begin.year) + corDate(date_begin.month) + corDate(date_begin.day)
-    d_now = str(date_now.year) + corDate(date_now.now().month) + corDate(date_now.day)
+    date_now = dateToString(date_required)
+    date_begin = dateToString(date_required + datetime.timedelta(days=-3))
 
     url_all = url + 'diag/block-detail?pid=11656&codes=' + key_code + '&codeType=stock&info=%7B%22view%22%3A%7B%22nolazy%22%3A1%2C%22parseArr%22%3A%7B%22_v%22%3A%22new%22%2C%22dateRange%22%3A%5B%22'
     url_all = url_all + d_begin + '%22%2C%22' + d_now + '%22%5D%2C%22staying%22%3A%5B%5D%2C%22queryCompare%22%3A%5B%5D%2C%22comparesOfIndex%22%3A%5B%5D%7D%2C%22asyncParams%22%3A%7B%22tid%22%3A9381%7D%7D%7D'
@@ -90,9 +99,12 @@ def getnews(search_key):
     return final_news
 
 def start():
-    stockCode = getZtCode()
-    dict = {}
-    for i in range(len(stockCode)):
-        dict[stockCode[i]] = getnews(stockCode[i])
-    return dict
+	stockCode = {}
+	dict = {}
+	
+	for d in range(0,5):
+		stockCode[dateToString(getDate(d))] = getZtCode(getDate(d))
+		for i in range(len(stockCode[dateToString(getDate(d))])):
+			dict[stockCode[dateToString(getDate(d))][i]] = getnews(stockCode[dateToString(getDate(d))][i],getDate(d))
+	print dict
 
