82c60b73766fdf8d3100ebc897bd6f754519ffda
AsrielMao <32787204+AsrielMao@users.noreply.github.com>
2017-11-07 10:05:04 +0800

    Update stock_news.py
    save date, stock code, news, URLs in 'stockNews. xls'

--- stock_news/stock_news.py
+++ stock_news/stock_news.py
@@ -7,17 +7,44 @@ import zt_crawler
 import url_crawler
 import origin_crawler
 import iwencai
+import xlwt
+import xlrd
+from xlutils.copy import copy
 
 unsaved = {}
+f = copy(xlrd.open_workbook('stockcode.xls'))
+sheet1 = f.sheets()[0]
+sheet2 = f.add_sheet(u'sheet2', cell_overwrite_ok=True)
+
 dict = iwencai.start()
+
+rowCounter1 = 0
+rowCounter2 = 0
+
 for (stock,list) in dict.items():
     print "dict[%s]=" % stock
+    columnCounter1 = 3
     for i in list:
         print i+'\n'
+        sheet1.write(rowCounter1,columnCounter1,i)
+        columnCounter1 = columnCounter1 + 1
+        columnCounter2 = 0
+        sheet2.write(rowCounter2,columnCounter2,i)
+        columnCounter2 = 1
+        
         # print unsaved
         news_urls = url_crawler.start(i)
+        
         for j in news_urls:
             # print j
             origin_crawler.origin_crawler(j,unsaved)
+            
+            sheet2.write(rowCounter2,columnCounter2,j)
+            columnCounter2 = columnCounter2 + 1
+        rowCounter2 = rowCounter2 + 1
+        
+    rowCounter1 = rowCounter1 + 1
+
+f.save('stockNewsO.xls')
 
 print unsaved
