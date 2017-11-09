b6f1484aca73084f3f2f04f52a16ff19f645d8ac
AsrielMao <32787204+AsrielMao@users.noreply.github.com>
2017-11-09 09:10:43 +0800

    Update origin_crawler.py

--- stock_news/origin_crawler.py
+++ stock_news/origin_crawler.py
@@ -7,6 +7,7 @@ from origin_xpath import dict
 import socket
 import chardet
 
+
 def origin_crawler(url, unsaved):
     global real_url, res
 
@@ -29,8 +30,8 @@ def origin_crawler(url, unsaved):
         print '超时'
         return
 
-    # print real_url,res
 
+    crawlerResult = []
 
     try:
         try:
@@ -40,7 +41,6 @@ def origin_crawler(url, unsaved):
 
         if dict[res].type == 'dynamic':
             print 1
-
         elif dict[res].type == 'static':
             print real_url
             req = urllib2.Request(real_url)
@@ -51,10 +51,10 @@ def origin_crawler(url, unsaved):
                 html0 = urllib2.urlopen(req, timeout=60).read()
                 charset = chardet.detect(html0)
                 html = html0.decode(charset['encoding'],'ignore')
-                print(charset['encoding'])
+                #print(charset['encoding'])
             else:
                 html = urllib2.urlopen(req, timeout=60).read().decode(dict[res].decoding,'ignore')
-                print(dict[res].decoding)
+                #print(dict[res].decoding)
 
             dom = etree.HTML(html)
             xpath_time = dict[res].time
@@ -66,6 +66,7 @@ def origin_crawler(url, unsaved):
                     origin = dom.xpath(xpath_origin)[0]
                     # print time, origin
                     print '\033[5;43m'+time, origin+' \033[0m!'
+                    crawlerResult=[time, origin]
                 else:
                     for i in range(len(xpath_time)):
                         temp_time = xpath_time[i]
@@ -75,6 +76,7 @@ def origin_crawler(url, unsaved):
                             origin = dom.xpath(temp_origin)[0]
                             # print time, origin
                             print '\033[5;43m' + time, origin + ' \033[0m!'
+                            crawlerResult=[time, origin]
                         except:
                             pass
 
@@ -83,24 +85,26 @@ def origin_crawler(url, unsaved):
                 if type(xpath_other) != list:
                     other = dom.xpath(xpath_other)[0]
                     print '\033[5;43m' + other + ' \033[0m!'
+                    crawlerResult=[other]
                 else:
                     for i in range(len(xpath_other)):
                         temp_other = xpath_other[i]
                         try:
                             other = dom.xpath(temp_other)[0]
                             print '\033[5;43m' + other + ' \033[0m!'
+                            crawlerResult=[other]
                         except:
                             pass
 
         else:
             print '数据库信息不完整，当前页面无法处理。'
-
     except KeyError:
         print '当前网站未收录，无法处理。 网站域名：' + res
         if unsaved.get(res) is not None:
             unsaved[res] = unsaved.get(res)+1
         else:
             unsaved[res] = 1
-
     except:
         print 'xpath有误' + res
+
+    return  crawlerResult
