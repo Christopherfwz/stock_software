aa9f6542351f9ea7e8d770329dee74c43c66687b
AsrielMao <32787204+AsrielMao@users.noreply.github.com>
2017-11-16 21:31:36 +0800

    Update stock_news.py

--- stock_news/stock_news.py
+++ stock_news/stock_news.py
@@ -17,25 +17,25 @@ sheet1 = stockOrigin.add_sheet(u'Sheet1', cell_overwrite_ok=True)
 
 rowCounter = 0
 
-
-dateStock = iwencai.getZtCode(iwencai.getDate(3))
-for ZtCodes in dateStock:
-	sheet1.write(rowCounter, 0, iwencai.dateToString(iwencai.getDate(3)))
-	sheet1.write(rowCounter, 1, ZtCodes)
-	
-	news = iwencai.getnews(ZtCodes, iwencai.getDate(3))
-	for exactnews in news:
-		sheet1.write(rowCounter, 2, exactnews)
+for d in range(0,10):
+	dateStock = iwencai.getZtCode(iwencai.getDate(d))
+	for ZtCodes in dateStock:
+		sheet1.write(rowCounter, 0, iwencai.dateToString(iwencai.getDate(d)))
+		sheet1.write(rowCounter, 1, ZtCodes)
 		
-		news_urls = url_crawler.start(exactnews)
-		for urls in news_urls:
-			originResults = origin_crawler.origin_crawler(urls,unsaved)
-			if originResults:
-				columnCounter = 3
-				for elemts in originResults:
-					sheet1.write(rowCounter, columnCounter, elemts)
-					columnCounter += 1
-				rowCounter +=1
-	
-				
+		news = iwencai.getnews(ZtCodes, iwencai.getDate(3))
+		for exactnews in news:
+			sheet1.write(rowCounter, 2, exactnews)
+			
+			news_urls = url_crawler.start(exactnews)
+			for urls in news_urls:
+				originResults = origin_crawler.origin_crawler(urls,unsaved)
+				if originResults:
+					columnCounter = 3
+					for elemts in originResults:
+						sheet1.write(rowCounter, columnCounter, elemts)
+						columnCounter += 1
+					rowCounter +=1
+
+
 stockOrigin.save('newsOrigin.xls')
